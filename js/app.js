d70806bc6c4c95c83bacf7d8ef71c0a535951379
betsyvies <ing.betsyvies@gmail.com>
2018-01-23 19:13:12 -0500

    añadiendo ubicación geografica

--- js/app.js
+++ js/app.js
@@ -1,26 +1,50 @@
-
-// La funcionalidad de tu proyecto
-
+/* Inicializamos con una funcion que contendra los eventos a utilizar */
+window.addEventListener('load', () => {
+  document.getElementById('find-me').addEventListener('click', search);
+});
 
 function initMap() {
-  let laboratoriaLima = {lat: -25.363, lng: 131.044};
+  let laboratoriaLima = {
+    lat: -12.1191427,
+    lng: -77.0349046
+  };
+
   let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 18,
     center: laboratoriaLima
   });
+
   let marker = new google.maps.Marker({
     position: laboratoriaLima,
     map: map
   });
-}
 
-function search() {
+  var infoWindow = new google.maps.InfoWindow({map: map});
+  
+  // Try HTML5 geolocation.
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(functionExito, functionError);
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+  
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
   }
 }
 
-var latitud, longitud;
-var functionExito = function(position) {
-}
 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+}
