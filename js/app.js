c96f0639f7912143a06d42e1701adf4374154bf4
Araceli Cueva <aycuevam@gmail.com>
2018-02-08 01:09:23 -0500

    subiendo autocompletado de inputs

--- js/app.js
+++ js/app.js
@@ -1,22 +1,37 @@
 
 function initMap() {
-  let laboratoriaLima = {lat: -25.363, lng: 131.044};
+  let laboratoriaLima = {lat: -12.1260837, lng: -77.0228761};
   let map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 18,
+    zoom: 12,
     center: laboratoriaLima
   });
   let marker = new google.maps.Marker({
     position: laboratoriaLima,
     map: map
   });
-}
 
-function search() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(functionExito, functionError);
+  // Autocompletar
+  let directionsService = new google.maps.DirectionsService;
+  let directionsDisplay = new google.maps.DirectionsRenderer;
+  directionsDisplay.setMap(map);//funcion para buscar la ruta en el mapa
+  let startAutoComp = (document.getElementById('start'));
+  let autocompliteStart = new google.maps.places.Autocomplete(startAutoComp);
+  autocompliteStart.bindTo('bounds', map);
+  let EndAutoComp = (document.getElementById('end'));
+  let autocompliteEnd = new google.maps.places.Autocomplete(EndAutoComp);
+  autocompliteEnd.bindTo('bounds', map);
+
+  function search() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(functionExito, functionError);
+    }
+  }
+
+  document.getElementById("encuentrame").addEventListener("click", search);
+
+  let latitud, longitud;
+  let  functionExito = function(position) {
   }
 }
 
-var latitud, longitud;
-var functionExito = function(position) {
-}
+
