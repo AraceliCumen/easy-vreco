b54e801d6675666fd0cc69a369c1202d67cdfd3c
betsyvies <ing.betsyvies@gmail.com>
2018-02-07 23:23:53 -0500

    añadiendo mapa

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-// La funcionalidad de tu proyecto
+
+function initMap() {
+  let laboratoriaLima = {lat: -25.363, lng: 131.044};
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: laboratoriaLima
+  });
+  let marker = new google.maps.Marker({
+    position: laboratoriaLima,
+    map: map
+  });
+}
+
+function search() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(functionExito, functionError);
+  }
+}
+
+var latitud, longitud;
+var functionExito = function(position) {
+}
