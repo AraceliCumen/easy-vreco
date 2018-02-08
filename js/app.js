79d3fe3fcadbcc0212b68f0363e271bf8fcf5a8c
melissayauri <yaurimelissa@gmail.com>
2018-02-07 23:05:27 -0500

    incorporando maps

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 // La funcionalidad de tu proyecto
+function initMap(){
+  var peru = { lat: -9.082632, lng: -84.0431127};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
+    center: peru
+  });
+}
