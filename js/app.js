2198057dff9b7bb9f6e91bef5836ed6f0cbae652
Araceli Cueva <aycuevam@gmail.com>
2018-02-08 02:00:06 -0500

    subiendo trazar ruta

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 // La funcionalidad de tu proyecto
-
  initMap = () => {
+  // variable para la ruta
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+
   let laboratoriaLima = {lat: -12.1260837, lng: -77.0228761};
   let map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
@@ -12,6 +15,8 @@
   });
 
 
+  //dibujar la ruta
+  directionsDisplay.setMap(map);
   // Autocompletar
   let startAutoComp = (document.getElementById('start'));
   let autocompliteStart = new google.maps.places.Autocomplete(startAutoComp);
@@ -19,5 +24,25 @@
   let EndAutoComp = (document.getElementById('end'));
   let autocompliteEnd = new google.maps.places.Autocomplete(EndAutoComp);
   autocompliteEnd.bindTo('bounds', map);
+
+  // evento boton trazar ruta
+  document.getElementById('ruta').addEventListener('click', () => { 
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  });
+}
+
+// calcular la ruta
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+  directionsService.route({
+      origin: document.getElementById('start').value,
+      destination: document.getElementById('end').value,
+      travelMode: 'DRIVING'
+  }, function (response, status) {
+      if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+      } else {
+          window.alert('Directions request failed due to ' + status);
+      }
+  });
 }
 
